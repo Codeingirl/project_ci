4cf5e90f020bc19bc3119b1a65804cec80fd1155
Codeingirl <biwnakoncm@gmail.com>
2020-01-29 17:54:20 +0700

    navbar

---
+++ application/views/main_view/navbar_m_view.php
@@ -0,0 +1,23 @@
+<body>
+	<!-- Page Preloder -->
+	<div id="preloder">
+		<div class="loader"></div>
+	</div>
+
+	<!-- Header section -->
+	<header class="header-section">
+		<a href="index.html" class="site-logo">
+			<img src="img/logo.png" alt="logo">
+		</a>
+		<ul class="main-menu">
+			<li><a href="index.html">Home</a></li>
+			<li><a href="characters.html">Characters</a></li>
+			<li><a href="game.html">Games</a></li>
+			<li><a href="reviews.html">Reviews</a></li>
+			<li><a href="news.html">News</a></li>
+			<li><a href="single-post.html">Page</a></li>
+		</ul>
+		<div class="header-add">
+			<img src="img/add.jpg" alt="">
+		</div>
+	</header>
