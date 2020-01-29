e157aad5f6c5bd3cbfc81116fec4797ce9db8b7a
Codeingirl <biwnakoncm@gmail.com>
2020-01-30 01:38:16 +0700

    sj_src

--- application/views/main_view/js_m_view.php
+++ application/views/main_view/js_m_view.php
@@ -1,12 +1,12 @@
 <!--====== Javascripts & Jquery ======-->
 	<script src="<?php echo base_url('css/admin_css/vendor/jquery-3.2.1.min.js');?>"></script>
 	<script src="<?php echo base_url('js/jquery-3.2.1.min.js');?>"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/jquery.slicknav.js"></script>
-	<script src="js/owl.carousel.min.js"></script>
-	<script src="js/circle-progress.min.js"></script>
-	<script src="js/jquery.magnific-popup.min.js"></script>
-	<script src="js/main.js"></script>
+	<script src="<?php echo base_url('js/bootstrap.min.js');?>"></script>
+	<script src="<?php echo base_url('js/jquery.slicknav.js');?>"></script>
+	<script src="<?php echo base_url('js/owl.carousel.min.js');?>"></script>
+	<script src="<?php echo base_url('js/circle-progress.min.js');?>"></script>
+	<script src="<?php echo base_url('js/jquery.magnific-popup.min.js');?>"></script>
+	<script src="<?php echo base_url('js/main.js');?>"></script>
 
 	</body>
 </html>
